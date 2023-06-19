9804751f6b7fe883a299efe6d5878d4b73fdd1a3
mohosinmiah <hamza1610330816@gmail.com>
2023-06-19 21:44:09 +0600

    sing @Mock and @InjectMocks annotations to mock the objec - Service Layer

--- src/test/java/com/testservicelayer/testservicelayer/service/EmployeeServiceTests.java
+++ src/test/java/com/testservicelayer/testservicelayer/service/EmployeeServiceTests.java
@@ -6,24 +6,30 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.BDDMockito;
-import org.mockito.Mockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.testservicelayer.testservicelayer.entity.Employee;
 import com.testservicelayer.testservicelayer.repository.EmployeeRepository;
 import com.testservicelayer.testservicelayer.service.Impl.EmployeeServiceImpl;
 
+@ExtendWith(MockitoExtension.class)
 public class EmployeeServiceTests {
     
+    @Mock
     private EmployeeRepository employeeRepository;
 
-    private EmployeeService employeeService;
+    @InjectMocks
+    private EmployeeServiceImpl employeeService;
 
     @BeforeEach
     public void setUp()
     {
-        employeeRepository = Mockito.mock(EmployeeRepository.class);
-        employeeService    = new EmployeeServiceImpl(employeeRepository);
+        // employeeRepository = Mockito.mock(EmployeeRepository.class);
+        // employeeService    = new EmployeeServiceImpl(employeeRepository);
     }
 
     // JQunit test for save employee opertaions
