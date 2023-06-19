a6241d7be2f14a5b5a2bb4b85d24022c038a0d7e
mohosinmiah <hamza1610330816@gmail.com>
2023-06-20 00:53:38 +0600

    EmployeeService getAllEmployees method - Positive Scenario

--- src/test/java/com/testservicelayer/testservicelayer/service/EmployeeServiceTests.java
+++ src/test/java/com/testservicelayer/testservicelayer/service/EmployeeServiceTests.java
@@ -1,5 +1,7 @@
 package com.testservicelayer.testservicelayer.service;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -12,6 +14,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.testservicelayer.testservicelayer.entity.Employee;
@@ -21,7 +24,7 @@ import com.testservicelayer.testservicelayer.service.Impl.EmployeeServiceImpl;
 
 @ExtendWith(MockitoExtension.class)
 public class EmployeeServiceTests {
-    
+
     @Mock
     private EmployeeRepository employeeRepository;
 
@@ -79,6 +82,42 @@ public class EmployeeServiceTests {
         });
         
     }
+
+    // JQunit test for get all employee operations
+    @Test
+    @DisplayName("JQunit test for getEmployeeList operations")
+    public void givenEmployeeObject_whenGetAllEmployee_thenReturnAllEmployee() {
+
+       // Given: Setup objects or preconditions
+        Employee employeeOne = Employee.builder()
+            .id(1L)
+            .firstName("MOHOSIN")
+            .lastName("MIAH")
+            .email("mohosinmiah1610@gmail.com")
+            .departmentCode("CSE")
+            .build();
+
+        Employee employeeTwo = Employee.builder()
+            .id(2L)  // Unique ID for the second employee
+            .firstName("JOHN")
+            .lastName("DOE")
+            .email("johndoe@gmail.com")
+            .departmentCode("IT")
+            .build();
+
+            // Mock the behavior of the employeeRepository
+            List<Employee> mockedEmployees = Arrays.asList(employeeOne, employeeTwo);
+            Mockito.when(employeeRepository.findAll()).thenReturn(mockedEmployees);
+
+            // When: Perform the action or behavior being tested
+            List<Employee> employees = employeeService.getEmployeeList();
+
+            // Then: Verify the output or expected result
+            assertThat(employees).isNotNull();
+            assertThat(employees).hasSize(2);
+            assertThat(employees).containsExactly(employeeOne, employeeTwo);
+    }
+
 
 
 }
